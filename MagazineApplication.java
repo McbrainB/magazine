752715995ff57c2b5248060b2c2d795ff308c0b0
McbrainB <67196581+McbrainB@users.noreply.github.com>
2021-02-18 23:10:51 +0100

    first commit

---
+++ MagazineApplication.java
@@ -0,0 +1,13 @@
+package com.example.magazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazineApplication.class, args);
+	}
+
+}
